cfe3664c1d68fd70365d123d687234cb7db2f06b
Andrew Salber <salber.andrew@gmail.com>
2022-07-17 14:15:10 -0700

    updated versions for mongoDB and mongoose

--- myApp.js
+++ myApp.js
@@ -2,7 +2,7 @@ require('dotenv').config({path: './sample.env'});
 const { Mongoose } = require('mongoose');
 var mongoose = require("mongoose");
 console.log(process.env.MONGO_URI)
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: false, useUnifiedTopology: true });
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true});
 
 let personSchema = new mongoose.Schema({
   name: {type: String, required: true},
